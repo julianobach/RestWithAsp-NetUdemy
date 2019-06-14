50597b0984ba91c4c800cb514a153037db5b53fb
julianobach <julianobach@gmail.com>
2019-06-14 12:31:26 -0700

    Implementing Diferent Verbs

---
+++ RestWithAspNetUdemy 02 - Using Diferent Verbs/RestWithAspNetUdemy/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAspNetUdemy.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string lastName { get; set; }
+        public string AddressName { get; set; }
+        public string Gender { get; set; }
+    }
+}
