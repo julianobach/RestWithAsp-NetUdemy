67cdf9272e7e5b6435b960b6949e7c329c0eeaf8
julianobach <julianobach@gmail.com>
2019-06-25 12:53:06 -0700

    Estudo generics

--- RestWithASPNETUdemy 06 - Adding Support To Database Migrations/RestWithASPNETUdemy/Startup.cs
+++ RestWithASPNETUdemy 06 - Adding Support To Database Migrations/RestWithASPNETUdemy/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.EntityFrameworkCore;
 using RestWithASPNETUdemy.Model.Context;
 using RestWithASPNETUdemy.Business;
 using RestWithASPNETUdemy.Business.Implementattions;
+using RestWithASPNETUdemy.Repository.Generic;
 
 namespace RestWithASPNETUdemy
 {
@@ -62,7 +63,11 @@ namespace RestWithASPNETUdemy
 
             //Dependency Injection
             services.AddScoped<IPersonBusiness, PersonBusinessImpl>();
+            services.AddScoped<IBookBusiness, BookBusinessImpl>();
+
             services.AddScoped<IPersonRepository, PersonRepositoryImpl>();
+
+            services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
